bcd8f1b0a686a93346be641842ba42edc994eb04
DrakyPym <darkericorbit.020802@gmail.com>
2023-06-11 14:22:46 -0600

    Comenzar

---
+++ build/CMakeFiles/SpaceWars.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SpaceWars.dir/SpaceWars.cpp.obj"
+  "CMakeFiles/SpaceWars.dir/SpaceWars.cpp.obj.d"
+  "SpaceWars.exe"
+  "SpaceWars.exe.manifest"
+  "SpaceWars.pdb"
+  "libSpaceWars.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SpaceWars.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

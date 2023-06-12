0d8b7717c57ec1c05ed683bd17f2f998b5dd6622
Eric <darkericorbit.020802@gmail.com>
2023-06-12 15:21:11 -0600

    .

--- include/tools.h
+++ include/tools.h
@@ -5,18 +5,8 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <dirent.h>
-#include <sys/stat.h>
-#include <sys/ioctl.h>
-#include <stdbool.h>
-#include <sndfile.h>
 
-
-#define MAX_PATH 4096 // Valor máximo para la ruta
-
-char getkey();
 void position(int x, int y);
+char getkey();
 
 #endif
--- src/SpaceWars.c
+++ src/SpaceWars.c
@@ -3,6 +3,7 @@
 #include "hideShow.h"
 
 int main(){
+    position(7, 15);
     printf("Hola mundo");
     return 0;
 }
